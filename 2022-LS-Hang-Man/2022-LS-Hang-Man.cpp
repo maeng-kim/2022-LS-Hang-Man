0b1a8ad756148728f394e0866375a00b055ea24f
ssohy <sohyeonj415@gmail.com>
2022-11-24 14:33:38 +0900

    commit11

--- 2022-LS-Hang-Man/2022-LS-Hang-Man.cpp
+++ 2022-LS-Hang-Man/2022-LS-Hang-Man.cpp
@@ -5,14 +5,16 @@ using namespace std;
 
 int main()
 {
+	
 	//게임에 필요한 영단어 배열 생성
 	string fruits[15]={"apple", "pear", "grapes", "mango", "watermelon", "orange", 
 "lime", "peach", "mandarin", "blueberry", "plum", "strawberry", "kiwi", "banana", "cherry"};
+	string alpabet = { 'a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','q','r' };
 
 	//단어 랜덤으로 선정
 	int index;
 	string dap;
-
+;
 	srand(time(0));
 	index = rand() % 15;
 	dap = fruits[index];
