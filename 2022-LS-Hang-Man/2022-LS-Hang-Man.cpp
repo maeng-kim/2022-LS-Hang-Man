99e04c062241d50093061da4599f85194e5f1815
ssohy <sohyeonj415@gmail.com>
2022-12-20 14:34:59 +0900

    commit

--- 2022-LS-Hang-Man/2022-LS-Hang-Man.cpp
+++ 2022-LS-Hang-Man/2022-LS-Hang-Man.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+char input();
+bool correct(char userChar, string dap);
 
 int main()
 {
@@ -9,10 +11,11 @@ int main()
 	//게임에 필요한 영단어 배열 생성
 	string fruits[15]={"apple", "pear", "grapes", "mango", "watermelon", "orange", 
 "lime", "peach", "mandarin", "blueberry", "plum", "strawberry", "kiwi", "banana", "cherry"};
-	string alpabet = { 'a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','q','r' };
+	string alpabet = { 'a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
 	//단어 랜덤으로 선정
-	int index;
+	int index, size;
+	char userChar;
 	string dap;
 ;
 	srand(time(0));
@@ -22,12 +25,41 @@ int main()
 	//게임 진행(행맨)
 	while (true)
 	{
+		size = dap.length();
+		//자리수출력
+		for(int i=0;i<size;i++)
+			cout << "_";
+
+		//유저문자입력
+		userChar=input();
+
+		//입력한 문자가 맞았을 시
+		if (correct(userChar, dap, size)) {
+
+		}
 
 	}
-
-
 	//최종결과 출력
 
 
 
+}
+
+//유저의 문자입력
+char input()
+{
+	char char1;
+	cout << "Enter a character : ";
+	cin >> char1;
+	return char1;
+}
+
+bool correct(char userChar, string dap, int size)
+{
+	for (int j = 0; j < size; j++) {
+		if (dap[j] == userChar)
+			return true;
+		else
+			return false;
+	}
 }
